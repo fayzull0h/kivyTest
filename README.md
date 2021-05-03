71cef4eeecc0777c14d410390255583810bfa47e
Fayzulloh <fayzulloh2004@gmail.com>
2021-05-03 19:53:44 -0400

    Add paint.py
    Deleted test.py, created paint.py, virt folder had some modifications too for some reason?

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 To run python files:
     source virt/bin/activate
     python3 [name-of-file]
-
--- test.py
+++
@@ -1,9 +0,0 @@
-import kivy
-from kivy.app import App
-from kivy.uix.button import Button
-
-class Test(App):
-	def build(self):
-		return Button(text="Hello")
-
-Test().run()
